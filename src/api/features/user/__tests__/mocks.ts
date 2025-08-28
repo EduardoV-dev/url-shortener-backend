fa9484613d49df428c900e0d14a5a7970ca925b4
Eduardo Varela <eduardovarela139@gmail.com>
2025-08-28 13:48:23 -0600

    feat(schema.prisma): update prisma schema for next version of the api

--- src/api/features/user/__tests__/mocks.ts
+++ src/api/features/user/__tests__/mocks.ts
@@ -7,6 +7,7 @@ export const MOCK_USER: User = {
   createdAt: new Date("2025-08-08T16:23"),
   email: "created@gmail.com",
   id: "1",
+  isAdmin: false,
   isDeleted: false,
   password: "hashed-password",
   updatedAt: new Date("2025-08-08T16:23"),
